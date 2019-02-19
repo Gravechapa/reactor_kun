630ecf0e745f1ddae2e5ec91f0e06ffb87f09373
Gravechapa <Gravechapa@protonmail.com>
2019-02-19 09:55:58 +0300

    Fix reactor parser build path

--- cmake_externalprojects/ReactorParser.cmake
+++ cmake_externalprojects/ReactorParser.cmake
@@ -19,12 +19,12 @@ if(NOT reactor_parser_POPULATED)
     include_directories("${reactor_parser_SOURCE_DIR}/headers")
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         add_custom_target(reactor_parser
-                    COMMAND cargo build --target-dir="${CMAKE_BINARY_DIR}/reactor_parser"
+                    COMMAND cargo build --target-dir="\"${CMAKE_BINARY_DIR}/reactor_parser\""
                     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/thirdparty/reactor_parser")
         set(REACTOR_PARSER_LIB "${CMAKE_BINARY_DIR}/reactor_parser/debug/libreactor_parser.so")
     else()
         add_custom_target(reactor_parser
-                    COMMAND cargo build --release --target-dir="${CMAKE_BINARY_DIR}/reactor_parser"
+                    COMMAND cargo build --release --target-dir="\"${CMAKE_BINARY_DIR}/reactor_parser\""
                     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/thirdparty/reactor_parser")
         set(REACTOR_PARSER_LIB "${CMAKE_BINARY_DIR}/reactor_parser/release/libreactor_parser.so")
     endif()
