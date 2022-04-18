0579facb0c0b504a1a8d0885bc2d794da63a543a
Gravechapa <Gravechapa@protonmail.com>
2022-04-18 05:16:56 +0300

    Update plog

--- apps/main.cpp
+++ apps/main.cpp
@@ -7,6 +7,8 @@
 #include <boost/stacktrace.hpp>
 #include <csignal>
 #include <plog/Log.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+#include <plog/Formatters/TxtFormatter.h>
 #include <plog/Appenders/ColorConsoleAppender.h>
 #include <filesystem>
 
